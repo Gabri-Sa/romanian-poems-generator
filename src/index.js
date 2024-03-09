af80e39e6e3eecc51f2651399b3fa6b2b15f2b03
Gabri-Sa <158731669+Gabri-Sa@users.noreply.github.com>
2024-03-09 12:20:45 +0000

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "A fost odata ca-n povesti",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "a4cf7a5b0a77537beftcb4bo13d400ab";
+  let prompt = `Generate a Romanian poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic poetry expert and love to write short poems.Your mission is to generate a 4 lines poem, and to make sure to input it in basic HTML format and separate each line with a <br />. Please mention the poem's author at the end of the poem, and then, after the author, two <br /> down, sign the poem with 'SheCodes AI' inside element in Italic.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 
   poemElement.innerHTML = "A fost odata ca-n povesti";
 }
