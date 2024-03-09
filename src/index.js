ad912977531e5ba144b91aee96f6b3bf49e5926f
Gabri-Sa <158731669+Gabri-Sa@users.noreply.github.com>
2024-03-09 11:15:51 +0000

    Added basic JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "A fost odata ca-n povesti",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+
+  poemElement.innerHTML = "A fost odata ca-n povesti";
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
