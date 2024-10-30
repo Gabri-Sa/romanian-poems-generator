2090ccb9bcac933a8bcd56562b4de15ddd5ccaf5
Gabriela Saftoiu <158731669+Gabri-Sa@users.noreply.github.com>
2024-10-30 16:42:08 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let apiKey = "a4cf7a5b0a77537beftcb4bo13d400ab";
   let prompt = `Generate a Romanian poem about ${instructionsInput.value}`;
   let context =
-    "You are a poetry expert and love to quote the most famous Romanian poets. Your mission is to generate a poem, to make sure to input this poem in HTML format, and separate each poem's line with a <br />. Please mention the poem's author at the end of the poem, and then, after the author, two <br /> down, sign the poem with 'SheCodes AI' inside element in Italic. Please do not display ```html ```  as text in poem's lines";
+    "You are a poetry expert and love to quote from the most famous Romanian poets. Your mission is to generate a poem, to make sure to input this poem in HTML format, and separate each poem's line with a <br />. Please mention the poem's author at the end of the poem, and then, after the author, two <br /> down, sign the poem with 'SheCodes AI' inside element in Italic. Please do not display ```html ```  as text in poem's lines";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
